897fb4f3d3369f19706f0c914efe7d79775cc555
Julio <julio_perez@ciencias.unam.mx>
2023-07-24 04:16:49 -0600

    esp now configurado

--- main/main.c
+++ main/main.c
@@ -16,12 +16,18 @@
 #include "esp_netif.h"
 #include "esp_mac.h"
 
+#define ESP_NOW_CHANNEL 1
+
 static const char *TAG = "Ecometro.";
+static uint8_t peer_mac[ESP_NOW_ETH_ALEN] = {0x58, 0xcf, 0xec, 0x62, 0x60, 0x83, 0x4b, 0xbc};
 
 static esp_err_t init_wifi(void);
 static esp_err_t init_esp_now(void);
+static esp_err_t register_peer(uint8_t *peer_adr);
+
 void receiveCallback(const esp_now_recv_info_t *esp_now_info, const uint8_t *data, int data_len);
 void sendCallback(const uint8_t *mac_addr, esp_now_send_status_t status);
+static esp_err_t enowSender(const uint8_t *peer_addr, const uint8_t *data, size_t len);
 
 /*-----------------------------------------------------------*/
 static esp_err_t init_wifi(void)
@@ -50,11 +56,14 @@ static esp_err_t init_esp_now(void)
 }
 /*-----------------------------------------------------------*/
 
+/*-----------------------------------------------------------*/
 void app_main(void)
 {
     init_wifi();
     init_esp_now();
+    ESP_ERROR_CHECK(register_peer(peer_mac));
 }
+/*-----------------------------------------------------------*/
 
 /*-----------------------------------------------------------*/
 void receiveCallback(const esp_now_recv_info_t *esp_now_info, const uint8_t *data, int data_len)
@@ -81,3 +90,19 @@ void sendCallback(const uint8_t *mac_addr, esp_now_send_status_t status)
     }
 }
 /*-----------------------------------------------------------*/
+
+/*-----------------------------------------------------------*/
+static esp_err_t register_peer(uint8_t *peer_adr)
+{
+    esp_now_peer_info_t peer_info = {};
+    memcpy(peer_info.peer_addr, peer_mac,ESP_NOW_ETH_ALEN);
+    peer_info.channel = ESP_NOW_CHANNEL;
+    peer_info.ifidx  = ESP_IF_WIFI_STA;
+    ESP_ERROR_CHECK(esp_now_add_peer(&peer_info));
+    return ESP_OK;
+}
+
+static esp_err_t enowSender(const uint8_t *peer_addr, const uint8_t *data, size_t len){
+    esp_now_send(&peer_addr,&data,len);
+    return ESP_OK;
+}
